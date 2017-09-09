508ae514444646eb0e61d9ed9f372300d0b1c57b
Yamid Pico Leal <ypicoleal@gmail.com>
2017-09-09 18:09:46 -0500

    icono de aplicacion y splash

---
+++ app/src/main/java/com/dranilsaarias/nad/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.dranilsaarias.nad
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
