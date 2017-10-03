94d379c2627adc3422b8254134930ac4dd367a9f
Yamid Pico Leal <ypicoleal@gmail.com>
2017-10-03 12:31:26 -0500

    interfaz tipo de consulta

--- app/src/main/java/com/dranilsaarias/nad/AgendarActivity.kt
+++ app/src/main/java/com/dranilsaarias/nad/AgendarActivity.kt
@@ -2,7 +2,9 @@ package com.dranilsaarias.nad
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_agendar.*
+import java.util.*
 
 
 class AgendarActivity : AppCompatActivity() {
@@ -14,6 +16,24 @@ class AgendarActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             finish()
         }
+
+        val type = findViewById<ClickToSelectEditText<Type>>(R.id.motivo)
+        val types = ArrayList<Type>()
+        types.add(Type("Consulta"))
+        types.add(Type("Depilación láser"))
+        types.add(Type("Otro procedimiento"))
+        types.add(Type("Láser"))
+        types.add(Type("Aplicación botox"))
+        types.add(Type("Otro motivo"))
+        type.setItems(types)
+        type.setOnItemSelectedListener(object : ClickToSelectEditText.OnItemSelectedListener<Type> {
+            override fun onItemSelectedListener(item: Type, selectedIndex: Int) {
+                Log.i("type", item.label)
+            }
+        })
     }
 
+    private inner class Type internal constructor(override val label: String) : Listable {
+
+    }
 }
