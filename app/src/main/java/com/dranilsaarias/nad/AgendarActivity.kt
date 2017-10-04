f7f5ae90ae81482aa368639564d31bc2799fe5ac
Yamid Pico Leal <ypicoleal@gmail.com>
2017-10-04 00:32:16 -0500

    interfaz tipo de consulta

--- app/src/main/java/com/dranilsaarias/nad/AgendarActivity.kt
+++ app/src/main/java/com/dranilsaarias/nad/AgendarActivity.kt
@@ -3,7 +3,11 @@ package com.dranilsaarias.nad
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.Transformation
 import kotlinx.android.synthetic.main.activity_agendar.*
+import kotlinx.android.synthetic.main.content_agendar.*
 import java.util.*
 
 
@@ -31,6 +35,58 @@ class AgendarActivity : AppCompatActivity() {
                 Log.i("type", item.label)
             }
         })
+
+        first.setOnClickListener {
+            openProgramingAnimation()
+        }
+    }
+
+    private fun openProgramingAnimation() {
+        //hours_container.visibility = View.GONE
+        //date_programming_container.visibility = View.VISIBLE
+
+        val a = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        a.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                hours_container.visibility = View.VISIBLE
+                date_programming_container.visibility = View.VISIBLE
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                hours_container.visibility = View.GONE
+                date_programming_container.visibility = View.VISIBLE
+            }
+        })
+
+        a.duration = 200
+        hours_container.startAnimation(a)
+    }
+
+    override fun onBackPressed() {
+        if (hours_container.visibility == View.GONE) {
+            closeProgramingAnimation()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun closeProgramingAnimation() {
+        hours_container.visibility = View.VISIBLE
+        date_programming_container.visibility = View.GONE
+
     }
 
     private inner class Type internal constructor(override val label: String) : Listable {
