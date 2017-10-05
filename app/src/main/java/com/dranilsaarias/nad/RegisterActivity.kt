1190f498ec5481f8b5e7734a4577aa73e00f3c5a
Yamid Pico Leal <ypicoleal@gmail.com>
2017-10-04 23:45:38 -0500

    Servicio de registro de usuario

--- app/src/main/java/com/dranilsaarias/nad/RegisterActivity.kt
+++ app/src/main/java/com/dranilsaarias/nad/RegisterActivity.kt
@@ -1,20 +1,32 @@
 package com.dranilsaarias.nad
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
 import android.util.Log
+import android.view.View
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.content_register.*
+import org.json.JSONObject
 import java.util.*
 
+
 class RegisterActivity : AppCompatActivity() {
+    var tipoDocumento = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         setSpinners()
         see_tos_btn.setOnClickListener { vetTos() }
+        register_btn.setOnClickListener { register() }
     }
 
 
@@ -24,14 +36,22 @@ class RegisterActivity : AppCompatActivity() {
         val entidad = findViewById<ClickToSelectEditText<Documento>>(R.id.entidad)
 
         val documentos = ArrayList<Documento>()
-        documentos.add(Documento("Cedula de ciudadania"))
-        documentos.add(Documento("Cedula de extrangeria"))
-        documentos.add(Documento("Pasaporte"))
-        documentos.add(Documento("Tarjeta de identidad"))
+        documentos.add(Documento("Cédula de ciudadanía"))
+        documentos.add(Documento("Tarjeta de indetidad"))
+        documentos.add(Documento("Cédula de extranjería"))
+        documentos.add(Documento("Registro Civil"))
         document.setItems(documentos)
         document.setOnItemSelectedListener(object : ClickToSelectEditText.OnItemSelectedListener<Documento> {
             override fun onItemSelectedListener(item: Documento, selectedIndex: Int) {
-                Log.i("documento", item.label)
+                Log.i("documento", "" + selectedIndex)
+                tipoDocumento = selectedIndex + 1
+                if (selectedIndex == 1 || selectedIndex == 3) {
+                    nombre_acudiente_container.visibility = View.VISIBLE
+                    cedula_acudiente_container.visibility = View.VISIBLE
+                } else {
+                    nombre_acudiente_container.visibility = View.GONE
+                    cedula_acudiente_container.visibility = View.GONE
+                }
             }
         })
 
@@ -76,7 +96,119 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    private inner class Documento internal constructor(override val label: String) : Listable {
+    private fun register() {
 
+        if (!accept_tos_cbr.isChecked) {
+            Snackbar.make(loading, "Debe aceptar los Términos y condiciones para poder registrarse", Snackbar.LENGTH_LONG)
+                    .setAction("Aceptar", {
+                        accept_tos_cbr.isChecked = true
+                    })
+                    .setActionTextColor(Color.WHITE)
+                    .show()
+            return
+        }
+
+        val serviceUrl = getString(R.string.register_form)
+        val url = getString(R.string.host, serviceUrl)
+
+        val request = object : StringRequest(Request.Method.POST, url,
+                Response.Listener<String> { response ->
+                    Log.e("tales", response)
+                    loading.visibility = View.GONE
+                },
+                Response.ErrorListener { error ->
+                    loading.visibility = View.GONE
+                    if (error.networkResponse != null && error.networkResponse.statusCode == 400) {
+                        showErrors(String(error.networkResponse.data))
+                    } else {
+                        Snackbar.make(loading, "Al parecer hubo un error en la peticion intentelo nuevamente mas tarde", Snackbar.LENGTH_LONG).show()
+                    }
+                }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params.put("username", numero_documento.text.toString())
+                params.put("password1", password.text.toString())
+                params.put("password2", password_confirm.text.toString())
+                params.put("first_name", nombre.text.toString())
+                params.put("last_name", apellidos.text.toString())
+                params.put("email", email.text.toString())
+                params.put("tipo", "" + tipoDocumento)
+                params.put("identificacion", numero_documento.text.toString())
+                params.put("fecha_nacimiento", fecha_nacimiento.text.toString())
+                params.put("estado_civil", civil.text.toString())
+                params.put("profesion", profesion.text.toString())
+                params.put("telefono", celular.text.toString())
+                return params
+            }
+        }
+        VolleySingleton.getInstance().addToRequestQueue(request, this)
+        clearErrors()
+        loading.visibility = View.VISIBLE
     }
+
+    private fun showErrors(serverResponse: String) {
+        Log.e("errors", serverResponse)
+        val errors = JSONObject(serverResponse)
+        if (errors.has("username")) {
+            val error = errors.getJSONArray("username").getString(0)
+            numero_documento_container.isErrorEnabled = true
+            numero_documento_container.error = error
+        }
+
+        if (errors.has("password1")) {
+            val error = errors.getJSONArray("password1").getString(0)
+            password_container.isErrorEnabled = true
+            password_container.error = error
+        }
+
+        if (errors.has("password2")) {
+            val error = errors.getJSONArray("password2").getString(0)
+            password_confirm_container.isErrorEnabled = true
+            password_confirm_container.error = error
+        }
+
+        if (errors.has("tipo")) {
+            val error = errors.getJSONArray("tipo").getString(0)
+            document_container.isErrorEnabled = true
+            document_container.error = error
+        }
+
+        if (errors.has("fecha_nacimiento")) {
+            val error = errors.getJSONArray("fecha_nacimiento").getString(0)
+            fecha_nacimiento_container.isErrorEnabled = true
+            fecha_nacimiento_container.error = error
+        }
+
+        if (errors.has("identificacion")) {
+            val error = errors.getJSONArray("identificacion").getString(0)
+            numero_documento_container.isErrorEnabled = true
+            numero_documento_container.error = error
+        }
+
+        if (errors.has("estado_civil")) {
+            val error = errors.getJSONArray("estado_civil").getString(0)
+            civil_container.isErrorEnabled = true
+            civil_container.error = error
+        }
+    }
+
+    private fun clearErrors() {
+        numero_documento_container.error = null
+        numero_documento_container.isErrorEnabled = false
+        password_container.error = null
+        password_container.isErrorEnabled = false
+        password_confirm_container.error = null
+        password_confirm_container.isErrorEnabled = false
+        document_container.error = null
+        document_container.isErrorEnabled = false
+        fecha_nacimiento_container.error = null
+        fecha_nacimiento_container.isErrorEnabled = false
+        numero_documento_container.error = null
+        numero_documento_container.isErrorEnabled = false
+        civil_container.error = null
+        civil_container.isErrorEnabled = false
+    }
+
+    private inner class Documento internal constructor(override val label: String) : Listable
 }
---
+++ app/src/main/java/com/dranilsaarias/nad/VolleySingleton.kt
@@ -0,0 +1,60 @@
+package com.dranilsaarias.nad
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+import java.net.CookieHandler
+import java.net.CookieManager
+import java.net.URI
+
+
+class VolleySingleton {
+    private var mRequestQueue: RequestQueue? = null
+    private var cookieManager: CookieManager? = null
+
+
+    private fun getRequestQueue(context: Context): RequestQueue? {
+        if (mRequestQueue == null) {
+            cookieManager = CookieManager()
+            CookieHandler.setDefault(cookieManager)
+
+            mRequestQueue = Volley.newRequestQueue(context.applicationContext)
+        }
+        return mRequestQueue
+    }
+
+    fun <T> addToRequestQueue(req: Request<T>, context: Context) {
+        getRequestQueue(context)!!.add(req)
+    }
+
+    internal fun cancelAll() {
+        mRequestQueue!!.cancelAll { request ->
+            Log.d("DEBUG", "request running: " + request.url)
+            true
+        }
+    }
+
+    internal fun getCookie(name: String, url: URI): String? {
+        val cookies = cookieManager!!.cookieStore.get(url)
+        for (cookie in cookies) {
+            if (cookie.name == name) {
+                return cookie.value
+            }
+        }
+        return null
+    }
+
+    companion object {
+        private var mInstance: VolleySingleton? = null
+
+        @Synchronized
+        fun getInstance(): VolleySingleton {
+            if (mInstance == null) {
+                mInstance = VolleySingleton()
+            }
+            return mInstance as VolleySingleton
+        }
+    }
+}
