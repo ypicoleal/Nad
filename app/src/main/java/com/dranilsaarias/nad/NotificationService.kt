c2f581a07b1ac74baca9aed1bea6e02700839573
Yamid Pico Leal <ypicoleal@gmail.com>
2017-11-14 23:41:55 -0500

    ajustes app

--- app/src/main/java/com/dranilsaarias/nad/NotificationService.kt
+++ app/src/main/java/com/dranilsaarias/nad/NotificationService.kt
@@ -1,17 +1,21 @@
 package com.dranilsaarias.nad
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
-
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+
 
 class NotificationService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage!!.from)
 
@@ -47,7 +51,26 @@ class NotificationService : FirebaseMessagingService() {
 
         // Check if message contains a notification payload.
         if (remoteMessage.notification != null) {
+            val bigText = NotificationCompat.BigTextStyle()
             Log.d(TAG, "Message Notification Body: " + remoteMessage.notification.body!!)
+            val builder = NotificationCompat.Builder(this, "NAD")
+
+            builder.setSmallIcon(R.drawable.ic_notificacion)
+                    .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_launcher))
+                    .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                    .setContentTitle(remoteMessage.notification.title)
+                    .setContentText(remoteMessage.notification.body)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setStyle(bigText)
+                    .setAutoCancel(true)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                builder.priority = NotificationManager.IMPORTANCE_HIGH
+            } else {
+                builder.priority = Notification.PRIORITY_MAX
+            }
+
+            val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            mNotificationManager.notify(0, builder.build())
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
